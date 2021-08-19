0db0215bd521334eb608e822703dc386b82b0f31
Vitor Santos <vitoralannl@gmail.com>
2021-08-19 11:57:34 -0300

    Criação do Controller e Model Curso

---
+++ app/Controllers/Curso.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controllers;
+
+class Home extends BaseController
+{
+	public function index()
+	{
+		return view('welcome_message');
+	}
+
+    public function get() {
+
+    }
+
+    public function getByName() {
+
+    }
+
+    public function post() {
+        
+    }
+}
